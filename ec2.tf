e95963aef7b873a1da786382a91992bec37ff298
MirIntern <97669345+MirIntern@users.noreply.github.com>
2022-02-16 05:32:23 -0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -45,6 +45,18 @@ resource "aws_launch_template" "t2microTemplateJenk" {
 	tags = {
 		name = "Jenkins"
 	}
+	user_data = <<EOT
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+	https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo amazon-linux-extras install epel -y
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+EOT
+
 }
 
 resource "aws_autoscaling_group" "minikubeServerScaleGrp" {
